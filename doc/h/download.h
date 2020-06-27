5baa3b55dce0a30c24ceabd90a21330f9e704c61
Guy Barrand <guy.barrand@gmail.com>
2020-06-27 16:45:31 +0200

    update

--- doc/h/download.h
+++
@@ -1,9 +0,0 @@
-/**
-@page inexlib_rio_download Download
-
-  Source and some binaries are on github :
-<p>
-  <a href="https://github.com/gbarrand/inexlib_rio.git" target="_blank">https://github.com/gbarrand/inexlib_rio.git</a>
-</p>
-
-*/
